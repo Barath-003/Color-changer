328de8e636090b8be82e2fa7850e5487185db1d8
Barath-003 <113625141+Barath-003@users.noreply.github.com>
2022-11-26 15:57:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Examm from './Examm';
+function App() {
+    return (
+        <div className="App">
+            <Examm />
+            
+        </div>
+    );
+}
+
+export default App;
